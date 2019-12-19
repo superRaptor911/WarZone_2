ba70c3f5d30a723919fa1e9d9bca059c0b7e9072
superRaptor911 <raptor.inc2018@gmail.com>
2019-12-19 20:59:52 +0530

    commit 1

--- Objects/Weapons/fireball.gd
+++ Objects/Weapons/fireball.gd
@@ -4,19 +4,23 @@ export var speed : float = 200
 
 var sender
 
+#play fire spwaning animation
 func _ready():
 	$AnimationPlayer.play("fire_beg")
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
+	#play on air animation
 	if anim_name == "fire_beg":
 		$AnimationPlayer.play("firebal")
+	#play flame out animation
 	elif anim_name == "fire_end":
 		queue_free()
 
 func _process(delta):
 	position += speed * transform.x * delta
 
+#set pos rot and sender while creating
 func create_fire_ball(pos,rot,sen):
 	position = pos
 	rotation = rot - 1.57
