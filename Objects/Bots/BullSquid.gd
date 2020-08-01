eada9d9bdbcaf6e792cba8e64f831af758643008
superRaptor911 <raptor.inc2018@gmail.com>
2020-08-01 20:01:25 +0530

    New zombie boss

--- Objects/Bots/BullSquid.gd
+++ Objects/Bots/BullSquid.gd
@@ -4,7 +4,27 @@ var custom_model = preload("res://Objects/Models/bull_sqid.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	model.queue_free()
-	model = custom_model.instance()
-	add_child(model)
-	model.connect("zm_attk_finished", self, "on_attk_completed")
+	pass
+
+# Override previous func defined in zombie.gd
+func P_on_killed():
+	$body.show()
+	$dtween.interpolate_property(self, "modulate", Color(1,1,1,1), Color(1,1,1,0), 2,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT, 6)
+	$dtween.start()
+
+func _on_navTimer_timeout():
+	getTarget()
+	var T = game_server._unit_data_list.get(target_id)
+	if T:
+		target_visible = isTargetVisible(T.ref)
+		if target_visible:
+			var dist = (T.ref.position - position).length()
+			if dist < 200:
+				if dist < 100:
+					rpc("zmAttack")
+				else:
+					rpc("acidAttack")
+					T.ref.takeDamage(melee_damage, "acid", "Zombie")
+
+remotesync func acidAttack():
+	model.spitAcid()
