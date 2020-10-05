4d3093262607553c5de85c083aa8d76c9be9de1e
superRaptor911 <raptor.inc2018@gmail.com>
2020-10-05 17:46:23 +0530

    pre-network update

--- Objects/Global/network.gd
+++ Objects/Global/network.gd
@@ -1,16 +1,17 @@
+# Network
 extends Node2D
 
+# Server advertiser for lan
 var serverAvertiser = null
-
-
+# List of connected players
 var players = {}
 
-
+# Signals
 signal server_created                          # when server is successfully created
 signal join_success                            # When the peer successfully joins a server
 signal join_fail                               # Failed to join a server
 signal player_list_changed                     # List of players has been changed
-signal player_removed(pinfo)
+signal player_removed(pinfo)				   # Called when player is removed
 signal disconnected
 signal server_stopped
 
@@ -22,22 +23,23 @@ func _ready():
 	get_tree().connect("connection_failed", self, "_on_connection_failed")
 	get_tree().connect("server_disconnected", self, "_on_disconnected_from_server")
 
+
 func _on_player_connected(_id):
 	pass
 
+# Called when player disconnects
 func _on_player_disconnected(id):
 	print("Player ", players[id].name, " disconnected from server")
-	if (get_tree().is_network_server()):
-		unregister_player(id)
+	if get_tree().is_network_server():
 		rpc("unregister_player", id)
 
 
+# Called when connected to server
 func _on_connected_to_server():
 	emit_signal("join_success")
 	Logger.Log("Connected to server")
 	game_states.player_info.net_id = get_tree().get_network_unique_id()
-	rpc_id(1, "register_player", game_states.player_info)
-	register_player(game_states.player_info)
+	rpc("register_player", game_states.player_info)
 
 
 func _on_connection_failed():
@@ -55,14 +57,15 @@ func _on_disconnected_from_server():
 	players.clear()
 	# Reset the player info network ID
 	game_states.player_info.net_id = 1
-	
 
+
+# Create server
 func create_server(server_name, port, max_players):
 	players.clear()
 	var net = NetworkedMultiplayerENet.new()
 	Logger.Log("Creating server %s on port %d" % [server_name,port])
 	
-	if (net.create_server(port,max_players) != OK):
+	if net.create_server(port,max_players) != OK:
 		Logger.Log("Failed to create server on port %d" % [port])	
 		return
 		
@@ -70,40 +73,41 @@ func create_server(server_name, port, max_players):
 	emit_signal("server_created")
 	Logger.Log("Loading Server Avertiser")
 	serverAvertiser = preload("res://Objects/Global/ServerAdvertiser.gd").new()
-	register_player(game_states.player_info)
+	# Register self
+	rpc("register_player", game_states.player_info)
+	# Set server info
 	game_server.serverInfo.port = String(port)
 	game_server.serverInfo.max_players = String(max_players)
 	game_server.serverInfo.name = server_name
-	game_server.serverInfo.plrs = String(players.size())
+	game_server.serverInfo.plrs = String(1)
 	
-	
+
+# Join server
 func join_server(ip, port):
 	Logger.Log("Connecting to server %s:%d" % [ip,port])
 	players.clear()
 	var net = NetworkedMultiplayerENet.new()
-	if (net.create_client(ip, port) != OK):
+	if net.create_client(ip, port) != OK:
+		Logger.Log("Failed to connect to %s:%s" % [ip, port])
 		emit_signal("join_fail")
-		Logger.Log("Connection failed")
 		return
 	Logger.Log("Connection successful")
+	Logger.Log("Connected to %s:%s" % [ip, port])
 	get_tree().set_network_peer(net)
 
-
-remote func register_player(pinfo):
-	if (get_tree().is_network_server()):
+# Register Player
+remotesync func register_player(pinfo):
+	# Server side
+	if get_tree().is_network_server():
 		if serverAvertiser:
 			serverAvertiser.serverInfo.plrs = String(players.size() + 1)
-		for id in players:
-			rpc_id(pinfo.net_id, "register_player", players[id])
-			if (id != 1):
-				rpc_id(id, "register_player", pinfo)
 
 	Logger.Log("Regestering player %s with id %d" % [pinfo.name, pinfo.net_id])
 	players[pinfo.net_id] = pinfo          # Create the player entry in the dictionary
 	emit_signal("player_list_changed")     # And notify that the player list has been changed
 
 
-remote func unregister_player(id):
+remotesync func unregister_player(id):
 	Logger.Log("Un-regestering player %s with id %d" % [players[id].name, players[id].net_id])
 	emit_signal("player_removed", players[id])
 	players.erase(id)
