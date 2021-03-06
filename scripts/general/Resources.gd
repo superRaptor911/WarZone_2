a2de102dfac942b1eb7293a4af84f760979b6986
superRaptor911 <raptor.inc2018@gmail.com>
2021-03-06 19:39:55 +0530

    Added Ak47

--- scripts/general/Resources.gd
+++ scripts/general/Resources.gd
@@ -6,19 +6,21 @@ var entities = {
 	}
 
 # bullet scenes
-var bullets = {
-	_9mm_fmj = preload("res://objects/bullets/9mm_fmj.tscn"),
-	_9mm_ap = preload("res://objects/bullets/9mm_fmj.tscn"),
-	}
+# var bullets = {
+# 	_9mm_fmj = preload("res://objects/bullets/9mm_fmj.tscn"),
+# 	_9mm_ap = preload("res://objects/bullets/9mm_fmj.tscn"),
+	# }
 
 # Guns here
 var guns = {
-	glock18 = preload("res://objects/guns/Glock18.tscn")
+	glock18 = preload("res://objects/guns/Glock18.tscn"),
+	ak47 = preload("res://objects/guns/Ak47.tscn"),
 	}
 
 # Gun sfx
 var gun_sounds = {
 	glock18 = [preload("res://resources/sound/sfx/weapons/glock18-1.wav"),preload("res://resources/sound/sfx/weapons/glock18-2.wav")],
+	ak47 = [preload("res://resources/sound/sfx/weapons/ak47-1.wav"), preload("res://resources/sound/sfx/weapons/ak47-2.wav")]
 	}
 
 # Skins
